e62b3284eeadff3792e6a1bfffc430965d5539b4
Julien Aubert <julien.aubert.mail@gmail.com>
2014-03-21 16:39:53 +0100

    fix make sure item has height/size in grid before we calculate item sizes

--- frontend/app/scripts/initialize.js
+++ frontend/app/scripts/initialize.js
@@ -4,32 +4,39 @@ require.register("scripts/initialize", function(exports, require, module) {
 
 
     ko.bindingHandlers.grid = {
-        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-            observe = function (){
-                var value = valueAccessor();
-                value({
-                    viewport: {
-                        width: $(element).width(),
-                        height: $(element).height()
-                    },
-                    itemport: {
-                        width: $(element).children(":first").outerWidth(true),
-                        height: $(element).children(":first").outerHeight(true)
-                    }
-                });
-            }
+        init: function(element, valueAccessor, allBindings, deprecated, bindingContext) {
+            items =  bindingContext.$data.items
 
-            // ensure accessor observes viewport-size and item-size once the grid-DOM is rendered
-            var subscription = allBindings().foreach.subscribe(function (newValue) {
-                observe();
-                subscription.dispose();
-            });
+            ko.applyBindingAccessorsToNode(element, {
+                foreach: function() { return bindingContext.$data.items },
+                event: function () { return { scroll: bindingContext.$data.scrolled } }
+            }, bindingContext);
+
+            observe_once_item_ready = function () {
+                item_ready = $(element).children(":first").outerHeight(true);
+                if (!item_ready) {
+                    setTimeout(observe_once_item_ready, 5);
+                } else {
+                    var value = valueAccessor();
+                    value({
+                        viewport: {
+                            width: $(element).width(),
+                            height: $(element).height()
+                        },
+                        itemport: {
+                            width: $(element).children(":first").outerWidth(true),
+                            height: $(element).children(":first").outerHeight(true)
+                        }
+                    });
+                }
+            }
+            observe_once_item_ready();
 
             // ensure accessor observes viewport-size and item-size when user resizes
             $(window).resize(function () {
-                observe();
+                observe_once_item_ready();
             });
-
+            return { controlsDescendantBindings: true };
         }
     }
 
