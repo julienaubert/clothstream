b3bf4f66fd68c60673f7528f336900749ab98bcf
Julien Aubert <julien.aubert.mail@gmail.com>
2014-03-21 14:04:08 +0100

    refactor: separate view-models

--- frontend/app/scripts/homeView.js
+++ frontend/app/scripts/homeView.js
@@ -8,19 +8,26 @@ require.register("scripts/homeView", function(exports, require, module) {
         this.link = ko.observable(data.link);
     }
 
-
-
-    exports.HomeViewModel = function() {
+    DiscoverView = function(template_name, item_repo) {
         var self = this;
-        self.items = ko.observableArray([]);
-        self.items.extend({ infinitescroll: {} });
-        self.item_by_dbid = {};
+        self.template_name = template_name;
+
+
         self.grid = ko.observable();
-        self.chosen_product = ko.observable(null);
-        self.active_view = ko.observable({});
+        self.items = item_repo.items;
+        self.item_by_dbid = item_repo.item_by_dbid;
+
+        self.load = function(params) {
+        }
+
+        self.context = {
+            items: item_repo.items,
+            grid: self.grid
+        };
 
         self.item_loader = new pageLoader.Sequential(
-            {   url: "http://localhost:8000/item/items/",
+            {
+                url: "http://localhost:8000/item/items/",
                 page_size: 20
             },
             function(props, page_loaded, data){
@@ -32,11 +39,6 @@ require.register("scripts/homeView", function(exports, require, module) {
                 self.items.valueHasMutated();
         });
 
-        self.go_to_product = function(product) {
-            console.log("go to prod: %o", product)
-            location.hash = getSlug(product.thumb_title) + "/" + product.id;
-        }
-
         self.scrolled = function(data, event) {
             var scroller = self.items.infinitescroll;
             var elem = event.target;
@@ -45,9 +47,6 @@ require.register("scripts/homeView", function(exports, require, module) {
         };
 
         self.grid.subscribe(function (grid) {
-            console.log("grid is doing stuff..")
-                    console.log("\tis g vis: ", self.active_view().home);
-
             var scroller = self.items.infinitescroll;
             scroller.viewportWidth(grid.viewport.width);
             scroller.viewportHeight(grid.viewport.height);
@@ -55,36 +54,73 @@ require.register("scripts/homeView", function(exports, require, module) {
             scroller.itemHeight(grid.itemport.height);
             self.item_loader.loadUntilEntry(scroller.lastVisibleIndex() + 1 + scroller.numItemsPadding());
         });
+        self.item_loader.load(1);
 
+    };
 
-        // ROUTING
+    ChosenProductView = function(template_name, item_repo) {
+        var self = this;
+        self.template_name = template_name;
+        self.items = item_repo.items;
+        self.item_by_dbid = item_repo.item_by_dbid;
+        self.chosen_product = ko.observable(null);
 
-        self.route_product = function(product_id) {
-            self.active_view({'product': true});
-            console.log("route product")
-            if (self.item_by_dbid[product_id]) {
-                self.chosen_product(self.item_by_dbid[product_id])
+        self.context = {
+            chosen_product: self.chosen_product
+        }
+
+        self.load = function(params) {
+            if (self.item_by_dbid[params.product_id]) {
+                self.chosen_product(self.item_by_dbid[params.product_id])
             } else {
-                $.get("http://localhost:8000/item/items/" + product_id + "/", function(result) {
-                    self.item_by_dbid[product_id] = result;
+                $.get("http://localhost:8000/item/items/" + params.product_id + "/", function(result) {
+                    self.item_by_dbid[params.product_id] = result;
                     self.chosen_product(result);
                 });
             }
-        };
+        }
+    };
 
-        self.route_home = function() {
-            self.active_view({'home': true});
-            console.log("route home")
-            self.chosen_product(null);
+
+    ItemRepository = function() {
+        var self = this;
+        self.items = ko.observableArray([]);
+        self.items.extend({ infinitescroll: {} });
+        self.item_by_dbid = {};
+    };
+
+    exports.HomeViewModel = function() {
+        var self = this;
+
+        // SHARED COMPONENTS
+        self.item_repo = new ItemRepository();
+
+
+        // VIEWS
+        self.views = {
+            'chosen_product': new ChosenProductView("chosen_product.html", self.item_repo),
+            'discover': new DiscoverView("discover.html", self.item_repo)
+        };
+        self.active_view = ko.observable();
+
+
+        // ROUTING
+        self.go_to_product = function(product) {
+            location.hash = getSlug(product.thumb_title) + "/" + product.id;
         }
 
+        self.route = function(view, data) {
+            view.load(data);
+            self.active_view(view);
+        }
 
         Sammy(function() {
-            this.get('#:productThumb/:productId', function() { self.route_product(this.params.productId); });
-            this.get('', function() { self.route_home(); });
+            this.get('#:productThumb/:productId', function() {
+                self.route(self.views.chosen_product, { product_id: this.params.productId });
+            });
+            this.get('', function() { self.route(self.views.discover) });
         }).run();
 
-        self.item_loader.load(1);
 
     };
 
